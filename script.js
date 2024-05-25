46639bea9000b21126f66345fe53ceb63a7860b9
chai-rin <163626929+chai-rin@users.noreply.github.com>
2024-05-25 07:14:48 +0000

    12주차 수업(script.js)

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('hello');
+//주석처리 commentary
+alert('HELLO');
+document.write('냥냥냥');
+var a=10;
+var b;
+b=100;
+console.log(a+b);
+console.log(5%3);
+//%은 나머지 구할 때
+console.log(7>3);
+console.log(7==7);
+//= 하나는 정의할 때, ==은 equal의 개념
+console.log(7!=7);
+console.log(7+parseInt('2',10));
+//parseInt : 숫자의 형태를 띄고 있는 글자값을 10진수로 변환
+console.log((3).toString());
+//toString : 숫자를 글자로
+var name = 'RIN';
+console.log(`안녕하세요, ${name} :)`);
+console.log('안녕하세요, ' +name);
+var p='cat dog hamster alligator';
+console.log(p.indexOf('dog'));
+//몇 번째인지 찾을 때 indexOf **숫자 0부터 시작 012345
